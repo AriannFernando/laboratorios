7c7111430123076f1ca28e4d7e8308091c0b7716
AriannFernando <A01703556@itesm.mx>
2022-03-12 15:56:44 -0600

    Cambios lab 17

---
+++ lab 17/routes/lab156.routes.js
@@ -0,0 +1,29 @@
+
+
+const express = require('express');
+
+const router = express.Router();
+
+const path = require('path');
+
+const lab156Controller = require('../controllers/lab156_controller');
+
+
+
+router.get('/', lab156Controller.intro); 
+
+router.post('/', lab156Controller.intro_post); 
+
+router.get('/lab1', lab156Controller.lab1_get); 
+
+router.post('/lab1', lab156Controller.lab1_post); 
+
+router.get('/lab5', lab156Controller.lab5); 
+
+router.get('/lab6', lab156Controller.lab6); 
+
+router.get('/respuesta', lab156Controller.respuesta); 
+
+
+
+module.exports = router;
